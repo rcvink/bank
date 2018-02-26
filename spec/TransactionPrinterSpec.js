3cf28fbfb269214aab078e8560f0a350ab664398
rcvink <rrvink@gmail.com>
2018-02-26 17:17:43 +0000

    Add transaction printer

---
+++ spec/TransactionPrinterSpec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+describe('TransactionPrinter', function() {
+  var transactionPrinter;
+  var deposit;
+  var withdrawal;
+
+  beforeEach(function() {
+    spyOn(console, 'log');
+    transactionPrinter = new TransactionPrinter();
+    deposit = {
+      date: function() { return new Date("January 4, 2012") },
+      amount: function() { return 100.00 },
+      isDeposit: function() { return true },
+      isWithdrawal: function() { return false }
+    };
+    withdrawal = {
+      date: function() { return new Date("January 4, 2012") },
+      amount: function() { return -200.00 },
+      isDeposit: function() { return false },
+      isWithdrawal: function() { return true }
+    };
+  });
+
+  it('prints a deposit correctly', function() {
+    transactionPrinter.print(deposit);
+    expect(console.log).toHaveBeenCalledWith('04/01/2012 || 100.00 || || ');
+  });
+
+  it('prints a withdrawal correctly', function() {
+    transactionPrinter.print(withdrawal);
+    expect(console.log).toHaveBeenCalledWith('04/01/2012 || || 200.00 || ');
+  });
+
+});
---
+++ src/TransactionPrinter.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function(exports) {
+  function TransactionPrinter() {
+
+  };
+
+  TransactionPrinter.prototype.print = function (transaction) {
+    console.log(`${this._date(transaction)} || ${this._amount(transaction)} || `);
+  };
+
+  TransactionPrinter.prototype._date = function (transaction) {
+    var day = String(transaction.date().getDate()).padStart(2, '0');
+    var month = String(transaction.date().getMonth() + 1).padStart(2, '0');
+    var year = transaction.date().getFullYear();
+    return `${day}/${month}/${year}`
+  };
+
+  TransactionPrinter.prototype._amount = function (transaction) {
+    if (transaction.isDeposit()) {
+      return `${transaction.amount().toFixed(2)} ||`;
+    } else if (transaction.isWithdrawal()) {
+      return `|| ${(-transaction.amount()).toFixed(2)}`;
+    };
+  };
+
+  exports.TransactionPrinter = TransactionPrinter;
+
+})(this);
